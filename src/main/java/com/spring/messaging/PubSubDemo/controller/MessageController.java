f63d88cec4bd998b332a7c5be06f633523a9d406
Tomi <juhasz.tamas991@gmail.com>
2021-08-19 18:38:11 +0200

    feat: controller skeleton

--- src/main/java/com/spring/messaging/PubSubDemo/controller/MessageController.java
+++ src/main/java/com/spring/messaging/PubSubDemo/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.spring.messaging.PubSubDemo.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/api/messages")
 public class MessageController {
-    
+
+    @PostMapping
+    public void publish() {
+        log.info("Publishing a message");
+    }
 }
