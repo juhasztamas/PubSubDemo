52ac1efb37a67ac8bc86e95ff99cf4c989edb1ef
Tomi <juhasz.tamas991@gmail.com>
2021-08-19 18:52:55 +0200

    refactor: responseentity in controller

--- src/main/java/com/spring/messaging/PubSubDemo/controller/MessageController.java
+++ src/main/java/com/spring/messaging/PubSubDemo/controller/MessageController.java
@@ -1,6 +1,8 @@
 package com.spring.messaging.PubSubDemo.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
     @PostMapping
-    public void publish() {
+    public ResponseEntity publish() {
         log.info("Publishing a message");
+        return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 
     @GetMapping
-    public void getAll() {
+    public ResponseEntity getAll() {
         log.info("Retrieving all stored messages");
+        return new ResponseEntity("messagesObject", HttpStatus.OK);
     }
 }
