1a6d53ed44db8aa5b8875a172ce1410a4363c3b4
Tomi <juhasz.tamas991@gmail.com>
2021-08-19 18:52:14 +0200

    feat: add model object

--- src/main/java/com/spring/messaging/PubSubDemo/model/Message.java
+++ src/main/java/com/spring/messaging/PubSubDemo/model/Message.java
@@ -1,2 +1,23 @@
-package com.spring.messaging.PubSubDemo.model;public class Message {
+package com.spring.messaging.PubSubDemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+import java.sql.Timestamp;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String content;
+
+    private Timestamp timestamp;
 }
