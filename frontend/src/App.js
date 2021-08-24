c4e89c378b6d3fadea83e724abacad7e1873b831
Tomi <juhasz.tamas991@gmail.com>
2021-08-24 23:05:55 +0200

    feat: add react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import SockJsClient from 'react-stomp';
+
+const WEBSOCKET_URL = 'http://localhost:8080/ws';
+const TOPIC = ['/topic/messages'];
+
+const App = () => {
+  const [messages, setMessages] = useState([]);
+
+  let onConnected = () => {
+    console.log("Connected!!")
+  }
+
+  let onMessageReceived = (msg) => {
+    console.log("messages received");
+    console.log(msg)
+    setMessages([
+      ...messages,
+      msg
+    ]);
+  }
+
+  return (
+    <div>
+      <SockJsClient
+        url={WEBSOCKET_URL}
+        topics={ TOPIC }
+        onConnect={onConnected}
+        onMessage={msg => onMessageReceived(msg)}
+      />
+      <ul>
+        {messages.map((msg, index) => (
+          <li key={index}>{ JSON.stringify( msg, null, 2 ) } </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
